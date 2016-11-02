e19490b65e996a92274e9633d4ca4b2f6fb21b81
Inna Pinsky <inna.pinsky@sap.com>
2016-11-02 13:15:48 +0000

    Smart T10b

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("T010bsmart.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("T010bsmart.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
